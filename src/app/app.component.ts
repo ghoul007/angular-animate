20f77398e1df3605ab0e3ef26a0e0916382bb85a
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2020-03-20 22:23:43 +0000

    handle event in angular animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -118,4 +118,13 @@ export class AppComponent {
   onShrink(){
   this.wildState = 'shrunken'
   }
+
+
+  animationStarted(event){
+    console.log(event)
+  }
+
+  animationEnded(event){
+    console.log(event)
+  }
 }
